5e21ce4f09f9e9b488b841ecb02a987b91c2c62f
lihanx <lihanx9@163.com>
2019-09-29 19:16:10 +0800

    update

--- _05_linked_list/linked_list.go
+++ _05_linked_list/linked_list.go
@@ -25,6 +25,32 @@ func NewLinkedLIst() *LinkedList {
 func (this *LinkedList) Insert(pos int, value interface{}) {}
 func (this *LinkedList) Delete(value interface{}) {}
 func (this *LinkedList) InsertBefore(posValue, newValue interface{}) {}
+
 func (this *LinkedList) DeleteBefore(posValue interface{}) {}
-func (this *LinkedList) Find(pos int) {}
-func (this *LinkedList) Index(value interface{}) {}
+
+func (this *LinkedList) Find(pos int) *Node {
+	if pos < 0 || pos >= this.Length {
+		return nil
+	}
+	node := this.Head
+	for i := -1; i < pos; i++ {
+		node = node.Next
+	}
+	return node
+}
+
+func (this *LinkedList) Index(value interface{}) int {
+	if value == nil {
+		return -1
+	}
+	node := this.Head
+	pos := 0
+	for node.Next != nil {
+		node = node.Next
+		if node.Value == value {
+			return pos
+		}
+		pos++
+	}
+	return -1
+}
