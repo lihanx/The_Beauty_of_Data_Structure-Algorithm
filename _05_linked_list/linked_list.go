c25cabbeccf46336579be0ee22c96fed03a264ad
lihanx <lihanx9@163.com>
2019-09-30 11:20:18 +0800

    update

--- _05_linked_list/linked_list.go
+++ _05_linked_list/linked_list.go
@@ -10,11 +10,13 @@ type LinkedList struct {
 	Length int
 }
 
+// 创建节点
 func NewNode(value interface{}) *Node {
 	node := Node{value, nil}
 	return &node
 }
 
+// 创建带头链表
 func NewLinkedLIst() *LinkedList {
 	newLinkedList := LinkedList{nil, 0}
 	node := NewNode(nil)
@@ -22,12 +24,61 @@ func NewLinkedLIst() *LinkedList {
 	return &newLinkedList
 }
 
-func (this *LinkedList) Insert(pos int, value interface{}) {}
-func (this *LinkedList) Delete(value interface{}) {}
-func (this *LinkedList) InsertBefore(posValue, newValue interface{}) {}
+// 插入节点
+func (this *LinkedList) Insert(idx int, value interface{}) int {
+	if idx < 0 || idx >= this.Length || value == nil {
+		return -1
+	}
+	current := this.Head
+	for i := 0; i < idx -1; i++ {
+		current = current.Next
+	}
+	node := NewNode(value)
+	node.Next = current.Next
+	current.Next = node
+	return idx
+}
 
-func (this *LinkedList) DeleteBefore(posValue interface{}) {}
+// 根据值删除节点
+func (this *LinkedList) Delete(value interface{}) *Node {
+	if value == nil || this.Length == 0 {
+		return nil
+	}
+	before := this.Head
+	for before.Next != nil {
+		current := before.Next
+		if current.Value == value {
+			before.Next = current.Next
+			current.Next = nil
+			return current
+		}
+		before = current
+	}
+	return nil
+}
 
+// 在指定节点之前插入节点
+func (this *LinkedList) InsertBefore(posValue, newValue interface{}) int {
+	if posValue == nil || newValue == nil || this.Length == 0 {
+		return -1
+	}
+	before := this.Head
+	idx := 0
+	for before.Next != nil {
+		current := before.Next
+		if current.Value == posValue {
+			node := NewNode(newValue)
+			node.Next = current
+			before.Next = node
+			return idx
+		}
+		before = current
+		idx++
+	}
+	return -1
+}
+
+// 根据index查找节点
 func (this *LinkedList) Find(pos int) *Node {
 	if pos < 0 || pos >= this.Length {
 		return nil
@@ -39,8 +90,9 @@ func (this *LinkedList) Find(pos int) *Node {
 	return node
 }
 
+// 查找指定节点的index
 func (this *LinkedList) Index(value interface{}) int {
-	if value == nil {
+	if value == nil || this.Length == 0 {
 		return -1
 	}
 	node := this.Head
